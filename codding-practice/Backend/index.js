8afc492bf3176f43c8b8630fb0b9b4b9e8f625bd
Balaram <balarammanadal2404@gmail.com>
2026-05-19 09:16:14 +0530

    added the docker

--- codding-practice/Backend/index.js
+++ codding-practice/Backend/index.js
@@ -1,23 +1,15 @@
-// Step 1 — Import http module
 const http = require("http");
 
-// Step 2 — Define host and port
-const HOST = "localhost";
 const PORT = 4000;
 
-// Step 3 — Create server
 const server = http.createServer((req, res) => {
-  
-  // Set response header
-  res.writeHead(200, { 
-    "Content-Type": "text/plain" 
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
   });
-  
-  // Send response
+
   res.end("Hello! Server is Running!");
 });
 
-// Step 4 — Start server
-server.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
